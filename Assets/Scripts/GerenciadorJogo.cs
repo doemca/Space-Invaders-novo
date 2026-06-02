01c99f115829aa124d1b85add9d21376c2c3aa49
doemca <gabriellealanaestudante@gmail.com>
2026-06-02 16:50:38 -0300

    Aula 02.06.26 (imcompleto)

--- Assets/Scripts/GerenciadorJogo.cs
+++ Assets/Scripts/GerenciadorJogo.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GerenciadorJogo : MonoBehaviour
@@ -7,10 +8,13 @@ public class GerenciadorJogo : MonoBehaviour
     public int inimigosADerrotar = 0;
     public int pontuacao = 0;
     public static GerenciadorJogo instance;
+    public TMP_Text textoPontos;
+    public GameObject TelaFimdeJogo;
 
     public void Start()
     {
         instance = this;
+        TelaFimdeJogo.SetActive(false);
     }
 
     public void Acrescentar(int pontos)
@@ -25,6 +29,17 @@ public class GerenciadorJogo : MonoBehaviour
 
     public void FimdoJogo()
     {
+        TelaFimdeJogo.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    public void Update()
+    {
+        textoPontos.SetText(pontuacao. ToString());
+    }
+
+    public void CarregarCena(string cena)
+    {
+        ScreenManager.LoadSceneAsync(cena);
     }
 }
